0e95c2c3d2ac6eff9eb370d8fe40e901a44495d7
zhouyu411502 <www.zhoubenyu.com@163.com>
2016-11-30 19:21:33 +0800

    初始化提交

---
+++ springboot-quick-commons/src/main/java/com/github/benyzhous/springboot/quick/commons/exception/MissingParameterException.java
@@ -0,0 +1,18 @@
+package com.github.benyzhous.springboot.quick.commons.exception;
+
+/**
+ * Created by chababa on 7/12/16.
+ */
+public class MissingParameterException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public MissingParameterException() {
+	}
+
+	public MissingParameterException(String message) {
+		super("Missing paramter:" + message);
+	}
+}
